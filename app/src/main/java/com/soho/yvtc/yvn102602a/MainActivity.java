bee5b2329de4dc8991a8127e571bc0a9b46683f7
pechentw <pechen.tw@gmail.com>
2016-10-26 20:37:38 +0800

    change spinner dispaly format add comments

--- app/src/main/java/com/soho/yvtc/yvn102602a/MainActivity.java
+++ app/src/main/java/com/soho/yvtc/yvn102602a/MainActivity.java
@@ -7,18 +7,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    String fruits[] = {"芭樂", "芒果", "荔枝", "甘蔗", "鳳梨"};
-    Spinner spinner;
-    ArrayAdapter<String> adapter;  //泛型 指定 adapter 資料的型態
+    String fruits[] = {"芭樂", "芒果", "荔枝", "甘蔗", "鳳梨"}; // 定義右側item
+    Spinner spinner; // 宣告左側
+    ArrayAdapter<String> adapter;  //泛型 指定 adapter 資料的型態  建立連結轉接器
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);  // 取得實體物件
 
-        adapter = new ArrayAdapter<String>(MainActivity.this,
+        adapter = new ArrayAdapter<String>(MainActivity.this,  // 建立轉接器與型式
                 android.R.layout.simple_list_item_1, // android.R.layout 為SDK內建程式建立的常數
                 fruits);
-        spinner.setAdapter(adapter);
+        spinner.setAdapter(adapter);  // 指定連結關係
     }
 }
