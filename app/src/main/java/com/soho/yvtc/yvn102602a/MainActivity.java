5fe7828be6bab11e27e6a9b11a43dd6f60990a4e
pechentw <pechen.tw@gmail.com>
2016-10-26 20:16:31 +0800

    Initial commit

---
+++ app/src/main/java/com/soho/yvtc/yvn102602a/MainActivity.java
@@ -0,0 +1,24 @@
+package com.soho.yvtc.yvn102602a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    String fruits[] = {"芭樂", "芒果", "荔枝", "甘蔗", "鳳梨"};
+    Spinner spinner;
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,
+                fruits);
+        spinner.setAdapter(adapter);
+    }
+}
